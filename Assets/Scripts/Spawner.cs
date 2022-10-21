a3c9eb4959594b2f4612e7c667a09ae330f88fc4
IgorPazukha <cskapervourlsk@mail.ru>
2022-10-22 00:52:28 +0500

    Init

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _object;
     [SerializeField] private int _objectCount;
+    [SerializeField] private Enemy _enemy;
+
     private int _minValue = -5;
     private int _maxValue = 5;
     private int _xPosition;
     private int _zPosition;
 
-    void Start()
+    private void Start()
     {
         StartCoroutine(Scatter());
     }
 
     private IEnumerator Scatter()
     {
+        int waitSecond = 2;
+        var waitTime = new WaitForSeconds(waitSecond);
+
         while (_objectCount > 0)
         {
             _xPosition = Random.Range(_minValue, _maxValue);
             _zPosition = Random.Range(_minValue, _maxValue);
-            Instantiate(_object, new Vector2(_xPosition, _zPosition), Quaternion.identity);
-            yield return new WaitForSeconds(2f);
+            Instantiate(_enemy, new Vector2(_xPosition, _zPosition), Quaternion.identity);
+
             _objectCount--;
+
+            yield return waitTime;
         }
     }
 }
